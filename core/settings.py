517b4b170eff1d9d21f424ccd8b177b7e2f63b45
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-11-13 17:27:48 +0100

    feat: adds docker configuration for db with postgresql

--- core/settings.py
+++ core/settings.py
@@ -59,13 +59,17 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_PORT"],
+        "TEST": {
+            "NAME": "test_wod_board",
+        },
     }
 }
 
