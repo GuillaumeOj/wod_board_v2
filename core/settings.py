7d7551c62003c46248f7ca3ee20500f3b98e4bc7
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-11-17 15:10:41 +0100

    feat: adds django-tailwind

--- core/settings.py
+++ core/settings.py
@@ -6,23 +6,16 @@ from dotenv import load_dotenv
 if os.environ.get("ENVIRONMENT") == "DEV":
     load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
+INTERNAL_IPS = ["127.0.0.1"]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     "users",
@@ -32,6 +25,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "tailwind",
+    "theme",
+    "django_browser_reload",
 ]
 
 MIDDLEWARE = [
@@ -42,6 +38,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -49,7 +46,8 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -60,6 +58,7 @@ TEMPLATES = [
         },
     },
 ]
+TAILWIND_APP_NAME = "theme"
 
 WSGI_APPLICATION = "core.wsgi.application"
 
@@ -96,22 +95,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("users.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
