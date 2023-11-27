7ab3882365894d025cda5b8f90811e93dc7dee74
Guillaume <7831777+GuillaumeOj@users.noreply.github.com>
2023-11-27 15:36:15 +0100

    chore: replace wods views with htmx templates

--- wods/views.py
+++ wods/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 
+from core.types import HtmxHttpRequest
 from wods.forms import WodForm
 from wods.models import Wod
 
@@ -10,7 +11,13 @@ from wods.models import Wod
 class WodListView(LoginRequiredMixin, ListView):
     model = Wod
     context_object_name = "wods"
-    template_name = "wods/list.html"
+    template_name: str = "wods/list_base.html"
+    request: HtmxHttpRequest
+
+    def get_template_names(self) -> list[str]:
+        if self.request.htmx:
+            return ["wods/list_partial.html"]
+        return [self.template_name]
 
     def get_queryset(self):
         return Wod.objects.filter(user=self.request.user)
@@ -19,10 +26,16 @@ class WodListView(LoginRequiredMixin, ListView):
 class WodCreateView(LoginRequiredMixin, CreateView):
     model = Wod
     form_class = WodForm
-    template_name = "wods/create.html"
+    template_name: str = "wods/create_base.html"
     success_url = reverse_lazy("home")
+    request: HtmxHttpRequest
 
-    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+    def get_template_names(self) -> list[str]:
+        if self.request.htmx:
+            return ["wods/create_partial.html"]
+        return [self.template_name]
+
+    def post(self, request: HtmxHttpRequest, *args, **kwargs) -> HttpResponse:
         self.object = None
         form = self.get_form()
         if not form.is_valid():
